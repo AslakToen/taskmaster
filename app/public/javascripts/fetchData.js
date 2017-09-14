3c53f5711368c15ef011908376fe6b38e32942bd
jotne-ast <aslak1ton@yahoo.no>
2017-09-14 17:53:05 +0200

    start of a new pwa
    added all the nonworking files

---
+++ app/public/javascripts/fetchData.js
@@ -0,0 +1,9 @@
+function getData(){
+  console.log('fetching...')
+		fetch('http://localhost:3000/nodefetch').then(function (data) {
+      console.log(data)
+			data.json().then(function (my_json) {
+				console.log(my_json);
+			});
+		});
+	}
---
+++ app/routes/nodefetch.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+con.connect();
+
+router.get('/', function(req, res, next) {
+  console.log("hello :3");
+  con.query('SELECT * FROM taskmanager.task', function(err, result, fields) {
+    if(err) throw err;
+    JSON.stringify(results);
+    res.send(results);
+  })
+});
+
+module.exports = router;
